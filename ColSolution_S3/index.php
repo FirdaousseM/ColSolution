1c16ce15cda8e05578a59527a052268444400308
asamassekou <83638793+asamassekou@users.noreply.github.com>
2021-12-22 20:58:46 +0100

    Update index.php

--- ColSolution_S3/index.php
+++ ColSolution_S3/index.php
@@ -8,6 +8,10 @@
 	require_once 'modules/mod_compte/mod_compte.php';
 	require_once 'modules/mod_recherches/mod_recherche.php';
 	require_once 'modules/mod_recherches/vue_recherche.php';
+	require_once 'modules/mod_accueil/mod_accueil.php';
+	require_once 'modules/mod_inscription/mod_inscription.php';
+	require_once 'modules/mod_modifCompte/mod_modifCompte.php';
+
 
     $vue = new Vue();
 	$vueR = new VueRecherche();
@@ -20,17 +24,19 @@
 		case "compte":
 			$mod = new ModCompte();
 			break;
-		case "recherche" :
+		case "recherche":
 			$mod = new ModRecherche();
 			break;
+		case "inscription":
+			$mod = new ModInscription();
+			break;
+		case "modif":
+			$mod = new ModModifCompte();
+			break;
+		default :
+			$mod = new ModAccueil();
 	}
-	if(!(isset($mod))){
-		$affichage = $vue->getAffichage();
-	}
-	else {
-
-	}
-
+	
 
 
 
