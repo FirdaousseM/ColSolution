165fd9ebfe38ec9bcc22dfb58f5ea0f6871e563a
asamassekou <83638793+asamassekou@users.noreply.github.com>
2021-12-25 23:42:54 +0100

    Update mod_recherche.php

--- ColSolution_S3/modules/mod_recherche/mod_recherche.php
+++ ColSolution_S3/modules/mod_recherche/mod_recherche.php
@@ -1,1 +1,37 @@
 
+<?php
+
+require_once 'cont_recherche.php';
+require_once 'modele_recherche.php';
+require_once 'vue.php';
+
+class ModRecherche{
+
+    public $controlerr;
+    public $modele;
+    public $mod;
+    public $vue;
+    public function __construct(){
+		$this->controlerr = new ContRecherche();
+        $this->modele = new ModeleRecherche();
+        $this->vue = new Vue();
+        switch($this->controlerr->action){
+            case "Users":
+                $this->controlerr->form_compte();
+                break;
+            case "Valider":
+                $this->controlerr->form_compte();
+                break;
+        }
+
+        if(!(empty($this->controlerr->ok)) AND isset($_GET['searc']) AND !empty($_GET['searc']) AND isset($_POST['Valider'])) {
+            $this->controlerr->form_compte();
+        }
+       
+    
+		
+	}
+
+}
+ 
+?>
