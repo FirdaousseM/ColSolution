8f1d723b6894e4dbc4bea98fbd0a08f1ae6d8aba
asamassekou <83638793+asamassekou@users.noreply.github.com>
2022-01-10 20:54:00 +0100

    Update vue_accueil.php

--- ColSolution_S3/modules/mod_accueil/vue_accueil.php
+++ ColSolution_S3/modules/mod_accueil/vue_accueil.php
@@ -1,38 +1,66 @@
-
 <?php 
-
-
+require_once 'modele_accueil.php';
 class VueAccueil {
-	
-	private $module;
-	private $action;
 
 	public function __construct() {
+		$modele = new ModeleAccueil();
+		$modele->cookie();
 		$this->affichage();
 	}
 
 	public function affichage() {
-		echo'
+	
+		$co = new Connexion();
+		$bdd = $co->initConnexion();
+		$annonce = $bdd->query('SELECT * FROM Annonce ORDER BY idAnnonce DESC LIMIT 4');
+		//$annonce = $bdd->query('SELECT * FROM Annonce natural join Image WHERE Annonce.idImage=Image.idImage ORDER BY idAnnonce DESC LIMIT 4');
+
+		$images = $bdd->query('SELECT * FROM Image natural join Annonce WHERE Image.idAnnonce=Annonce.idAnnonce ORDER BY idImage DESC');		
+
+		if (isset($_GET['annonce']) AND !empty($_GET['annonce'])) {
+			$search = htmlspecialchars($_GET['annonce']);
+			$annonce = $bdd->query('SELECT * FROM Annonce Where titre LIKE "%'.$search.'%" ORDER BY idAnnonce DESC LIMIT 4 ');
+	
+			$images = $bdd->query('SELECT * FROM Image inner join Annonce using(idAnnonce) WHERE Annonce.titre LIKE "%'.$search.'%" ORDER BY idAnnonce DESC LIMIT 4');		
+
+		}
+
+		?>
 		
 		<!-- PAGE ACCUEIL COLSOLUTION -->
-
 		<HTML>
-
 			<HEAD>
 				<TITLE> Colsolution </TITLE>
 				<META CHARSET="UTF-8">
-				<link href="css/style.css" rel="stylesheet" type="text/css" />
+				<link href="css/accueil.css" rel="stylesheet" type="text/css" />
 			</HEAD>
-			
+		
+
 			<BODY>
-			
+
+			<div id = "menu">
+				<h2>FILTRES</h2>
+				<div class="button_Composant" >
+					<div id="b1">
+					<a id="cb" href="index.php?module=recherche&action=zone">ZONE</a>
+					</div>
+					<div id="b2">
+					<a id="cb" href="index.php?">RECOMMENDATIONS</a>
+					</div>
+					<div id="b3">
+					<a id="cb" href="index.php?">COMPTE</a>
+					</div>
+				</div>
+				</div>
+
 				<!-- en tête de page-->
-				<p id="pp"> Vous recherchez un colocataire? Vous êtes au bon endroit </p> 
-
+				</br>
+				<p id="pp"> Vous recherchez un colocataire ? Vous êtes au bon endroit </p> 
 				<HEADER>
-					<a href="modules/mod_accueil/accueil.php"> <img class="logo" src="images/logo2.png" alt="Logo du site"/> </a>
-
-					<input id="searchbar" type="text" name="search" placeholder="Recherche..." />
+					<a href="index.php"> <img class="logo" src="images/COL.png" alt="Logo du site"/> </a>
+					<form id="searchbar" method = "GET">
+					<input id="searchbar" type="search" name="annonce" placeholder="Recherche..." />
+					</form>
 				</HEADER>
 				
 				<!-- menu de navigation -->
@@ -41,138 +69,85 @@ class VueAccueil {
 				
 					<nav id="menusMessage">
 					
-						<a class="menuLink" href="message.php">  <img class="icons" src="images/icons/messa.png" alt=""/>Messages </a>
+						<a class="menuLink" href="index.php?module=message&action=message">  <img class="icons" src="images/icons/messa.png" alt=""/>Messages </a>
 					
 					</nav>
 					<nav id="Annonce">
 					
-						<a class="menuLink" href="index.php?module=mod_annonce&&action=newAnnonce"> <img class="icons" src="images/icons/map-marker.png" alt=""/> Deposer Annonce </a>
-						<a class="menuLink" href="recherche.php"> <img class="icons" src="images/sr.png" alt=""/> Rechercher Annonce </a>				
-					
+						<a class="menuLink" href="index.php?module=annonce&&action=depotAnnonce"> <img class="icons" src="images/icons/map-marker.png" alt=""/> Deposer Annonce </a>
+						<a class="menuLink" href="index.php?module=recherche&action=Users"> <img class="icons" src="images/sr.png" alt=""/> Utilisateurs </a>				
 					</nav>
-					
 					<nav id="menusCompte">
 					
-						<a class="menuLink" href="index.php?module=mod_compte"> <img class="icons" src="images/icons/favicon.ico" alt=""/> Mon Compte </a>
-					
+						<a class="menuLink" href="index.php?module=inscription&action=inscription"> <img class="icons" src="images/icons/favicon.ico" alt=""/> Mon Compte </a>
 					</nav>
-					
-					
-				
-				
 				</NAV>
-				
-				
+
+		
+
+
 				<!-- corps de page -->
-				
-				
 				<MAIN>
 					<SECTION>
 					
 						<ARTICLE>
-						
+							<?php
+						if (empty($_GET['annonce'])) { ?>
 							<h2> DERNIERES ANNONCES </h2>
-							
-							<div id="an1"> 
-							
-								<div id="an1_pic">
-							
-									<a href="DepoAnnonce.php"> <img class="an" src="images/annoncee.png" alt="annonce"/> </a>
-								</div>
-
-								<div id="an1_text">
-								
-									<h3> TITRE ANNONCE 1</h3>
-								
-									<p> BLABLABLABLABLABLABLABLABLALBABLABLA  </p> <a href="DepoAnnonce.php"> [Lire la suite] </a>
-								
-								</div>
-							
-							</div>
-							
-							<div id="an2">
-							
-								<div id="an2_pic">
-							
-									<a href="DepoAnnonce.php"> <img class="ann" src="images/annoncee.png" alt="an2"/> </a>
-								</div>
-
-								<div id="an2_text">
-								
-								<h3> TITRE ANNONCE 2 </h3>
-							
-								<p> BLABLABLABLABLABLABLABLABLALBABLABLA </p> <a href="DepoAnnonce.php"> [Lire la suite] </a>
-							
-							</div>
-							</div>
-							<div id="an3">
-								<div id="an3_pic">
-							
-									<a href="DepoAnnonce.php"> <img class="an" src="images/annoncee.png" alt="an3"/> </a>
-								</div>
-
-								<div id="an3_text">
-								
-								<h3> TITRE ANNONCE 3 </h3>
-							
-								<p> BLABLABLABLABLABLABLABLABLALBABLABLA </p> <a href="DepoAnnonce.php"> [Lire la suite] </a>
-							
-							</div>
-							
-							</div>
-							
-							
-							<div id="an4">
-							
-								<div id="an4_pic">
-							
-									<a href="DepoAnnonce.php"> <img class="ann" src="images/annoncee.png" alt="an4"/> </a>
-								</div>
-
-								<div id="an4_text">
-								
-								<h3> TITRE ANNONCE 4 </h3>
-							
-								<p> BLABLABLABLABLABLABLABLABLALBABLABLA </p> <a href="DepoAnnonce.php"> [Lire la suite] </a>
-							
-							</div>
-							
-							</div>
-							
-							
-						
+						<?php } else {?> <h2> RESULTAT POUR "<?= $search ?>" </h2> 
+					<?php
+							}if($annonce->rowCount() > 0) {
+                ?>
+                    <?php while($annonces = $annonce->fetch() AND $image = $images->fetch()) {
+                    ?>
+				 <div id="an1">
+						<div id="an1_pic"> 
+							<?php
+							if($image['nomImage'] == null ) { ?>
+						<a href="DepoAnnonce.php"> <img class="an" src="images/annoncee.png" alt="annonce"/> </a>
+							<?php } ?>
+						<?php
+						if($image['nomImage'] != null ) { ?>
+							<a href="DepoAnnonce.php"> <img class="an" src="../public/annonces/<?php echo $image['nomImage'];?>" alt="annonce"/> </a>
+							<?php } ?>
+						</div>
+                        <div id="an1_text">
+							<h3><?= $annonces['titre']; ?> </h3>
+							<p> <?= $annonces['description']; ?> </p> <a href="index.php?module=annonce&action=depotAnnonce" >  </a>
+						</div>
+					</div>
+                    <?php
+					} }?>
 						</ARTICLE>
-					
 					</SECTION>
 				</MAIN>
-				
-				
+
+
 				<!-- footer -->
-				
-				
-				<FOOTER>
-				
-					<a href="modules/mod_accueil/accueil.php"> <img src="images/logo.png" alt="Logo du site"/> </a>
-					
-					
-					<p> 2021 - COLSOLUTION - Creative Common Licence</p>
+				<FOOTER> 				
+					<a href="index.php"> <img src="images/COL.png" alt="Logo du site"/> </a>				
+					<p id ="fp" > 2021 - COLSOLUTION - Creative Common Licence </p>;
 
+				<?php
+					if(!isset($_COOKIE['accepter'])) {
+				?>
+					<div class = "banniere">
+							 <div class = "text_banniere">
+							 <p> Bienvenue sur COLSOLUTION, notre site utilise des cookies pour une meilleure expérience </p>
+							 </div>
+							 <div class="button_banniere" >
+								<a id="tb" href="index.php?cookie=accepter">Tout accepter et continuer</a>
+							</div>
+							</div>
+					<?php
+					 }
+					 ?>
 				
-				</FOOTER>
-			
-			
-			
-			
-			
+				</FOOTER> 
 			</BODY>
+		</HTML>
 
-
-		</HTML>';
+<?php
 	}
-
-
-
 }
-		
 ?>
-
