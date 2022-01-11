9b5aff75548d228cd09b8deba4da9c5a3dd4675f
FirdaousseM <83703973+FirdaousseM@users.noreply.github.com>
2022-01-11 10:42:35 +0100

    Update mod_compte.php

--- ColSolution_S3/modules/mod_compte/mod_compte.php
+++ ColSolution_S3/modules/mod_compte/mod_compte.php
@@ -1,22 +1,29 @@
+
 <?php
 
 require_once 'cont_compte.php';
+require_once 'modele_compte.php';
+require_once 'vue.php';
 
+ini_set('xdebug.max_nesting_level', 5000);
 class ModCompte{
 
-  public $controlerr;
-
-  public function __construct(){
+    public $controlerr;
+    public $modele;
+    public $mod;
+    public $vue;
+    public function __construct(){
 		$this->controlerr = new ContCompte();
-        
-    switch($this->controlerr->action){
-      case "compte":
-        $this->controlerr->form_compte();
-      break;
-        
-      case "deconnexion" :
-        $this->controlerr->deconnexion();
-      break;
+        $this->modele = new ModeleCompte();
+        $this->vue = new Vue();
+        switch($this->controlerr->action){
+            case "compte":
+                $this->controlerr->form_compte();
+                break;
+              case "deconnexion" :
+                $this->controlerr->deconnexion();
+               // header('Location:index.php');
+
         }
 		
 	}
